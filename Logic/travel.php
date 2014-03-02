2dbf9f8385bd499532f14bf0c55527c18253795c
Bryce Freshcorn <bcf4kv@virginia.edu>
2014-03-02 08:11:54 -0500

    commit 14

--- Logic/travel.php
+++ Logic/travel.php
@@ -1,98 +1,93 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Travel App</title>
-		<link href="css/travelStyle.css" rel="stylesheet">
-		<!-- Bootstrap -->
-	    <!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
-	</head>
-	<body>
-		<?php
-		$address = $_POST['addressInput'];
-		$city = $_POST['cityInput'];
-		$country = $_POST['countryInput'];
+<?php
+	$address = $_POST['addressInput'];
+	$city = $_POST['cityInput'];
+	$country = $_POST['countryInput'];
 
-		$userDest = $address . ", " . $city . ", " . $country;
-		$begGeoURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-		$begGeoURL = $begGeoURL . urlencode($begGeoURL) . "&sensor=false";
+	echo $city . " and " . $country;
 
-		$goToSite = curl_init();
-		curl_setopt($goToSite, CURLOPT_URL, $begGeoURL);
-		curl_setopt($goToSite, CURLOPT_RETURNTRANSFER, 1);
-		$locData = json_decode(curl_exec($goToSite), true);
+	$userDest = $address . ", " . $city . ", " . $country;
+	$begGeoURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+	$begGeoURL = $begGeoURL . urlencode($begGeoURL) . "&sensor=false";
 
-		$latitude = $locData["results"][0]["geometry"]["lat"];
-		$longitude = $locData["results"][0]["geometry"]["lng"];
+	$goToSite = curl_init();
+	curl_setopt($goToSite, CURLOPT_URL, $begGeoURL);
+	curl_setopt($goToSite, CURLOPT_RETURNTRANSFER, 1);
+	$locData = json_decode(curl_exec($goToSite), true);
 
-		echo "<p id='#hideLat'>" . $latitude . "</p>";
-		echo "<p id='#hideLon'>" . $longitude . "</p>";
-		?>
+	$latitude = $locData["results"][0]["geometry"]["lat"];
+	$longitude = $locData["results"][0]["geometry"]["lng"];
 
+echo "<html lang='en'>";
+	echo "<head>";
+		echo "<meta charset='utf-8'>";
+		echo "<meta http-equiv='X-UA-Compatible' content='IE=edge'>";
+		echo "<meta name='viewport' content='width=device-width, initial-scale=1'>";
+		echo "<title>Travel App</title>";
+		echo "<link href='../css/travelStyle.css' rel='stylesheet'>";
+		echo "<!-- Bootstrap -->";
+	    echo "<link href='../css/bootstrap.min.css' rel='stylesheet'>";
+	echo "</head>";
+	echo "<body>";
+	echo "<h1>You chose " . $country . "</h1>";
+	echo "<div id='weatherDiv'>";
+	echo "<p>Weather in " . $city . ", " . $country . "</p>";
+	echo "</div>";
+		echo "<div id='twoDivs'>";
+			echo "<div id='poiDiv'>";
+				echo "<p>Points of Interest</p>";
+				echo "<table id='poiTable'>";
+					echo "<tr>";
+  						echo "<th>Name</th>";
+  						echo "<th>Address</th>";		
+  						echo "<th></th>";
+  					echo "</tr>";
+					echo "<tr>";
+					echo "<tr>";
+  						echo "<td></td>";
+  						echo "<td></td>";		
+  						echo "<td></td>";
+  					echo "</tr>";
+					echo "<tr>";
+  						echo "<td></td>";
+  						echo "<td></td>";		
+  						echo "<td></td>";
+					echo "</tr>";
+					echo "<tr>";
+  						echo "<td></td>";
+  						echo "<td></td>";	
+  						echo "<td></td>";
+					echo "</tr>";
+				echo "</table>";
+			echo "</div>";
 
-		<h1>Results Page</h1>
-		<h2>You chose &ltInsert Country Here&gt</h2>
+			echo "<div id='restaurantsDiv'>";
+				echo "<p>Nearby Restaurants</p>";
+				echo "<table id='restaurantsTable'>";
+					echo "<tr>";
+  						echo "<th>Name</th>";
+  						echo "<th>Address</th>";		
+  						echo "<th></th>";
+  					echo "</tr>";
+					echo "<tr>";
+					echo "<tr>";
+  						echo "<td></td>";
+  						echo "<td></td>";		
+  						echo "<td></td>";
+  					echo "</tr>";
+					echo "<tr>";
+  						echo "<td></td>";
+  						echo "<td></td>";		
+  						echo "<td></td>";
+					echo "</tr>";
+					echo "<tr>";
+  						echo "<td></td>";
+  						echo "<td></td>";	
+  						echo "<td></td>";
+					echo "</tr>";
+				echo "</table>";
+			echo "</div>";
+		echo "</div>";
 
-		<div id="weatherDiv">
-			<p>Weather in Charlottesville</p></div>
-
-		<div id="twoDivs">
-			<div id="poiDiv">
-				<p>Points of Interest</p>
-				<table id="poiTable">
-					<tr>
-  						<th>Name</th>
-  						<th>Address</th>		
-  						<th></th>
-  					</tr>
-					<tr>
-					<tr>
-  						<td></td>
-  						<td></td>		
-  						<td></td>
-  					</tr>
-					<tr>
-  						<td></td>
-  						<td></td>		
-  						<td></td>
-					</tr>
-					<tr>
-  						<td></td>
-  						<td></td>		
-  						<td></td>
-					</tr>
-				</table>
-			</div>
-
-			<div id="restaurantsDiv">
-				<p>Nearby Restaurants</p>
-				<table id="restaurantsTable">
-					<tr>
-  						<th>Name</th>
-  						<th>Address</th>		
-  						<th></th>
-  					</tr>
-					<tr>
-					<tr>
-  						<td></td>
-  						<td></td>		
-  						<td></td>
-  					</tr>
-					<tr>
-  						<td></td>
-  						<td></td>		
-  						<td></td>
-					</tr>
-					<tr>
-  						<td></td>
-  						<td></td>		
-  						<td></td>
-					</tr>
-				</table>
-			</div>
-		</div>
-
-	</body>
-</html>
+	echo "</body>";
+echo "</html>";
+?>
