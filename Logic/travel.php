7bb5a77c9c9460452f9b3ba4145cd00abf7223ff
Lucy Graham <lbg8zw@virginia.edu>
2014-03-02 06:18:10 -0500

    Commit 11

--- Logic/travel.php
+++ Logic/travel.php
@@ -10,6 +10,28 @@
 	    <!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
 	</head>
 	<body>
+
+	<?php
+		$address = $_POST['addressInput'];
+		$city = $_POST['cityInput'];
+		$country = $_POST['countryInput'];
+
+		$userDest = $address . ", " . $city . ", " . $country;
+		$begGeoURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+		$begGeoURL = $begGeoURL . urlencode($begGeoURL) . "&sensor=false";
+
+		$goToSite = curl_init();
+		curl_setopt($goToSite, CURLOPT_URL, $begGeoURL);
+		curl_setopt($goToSite, CURLOPT_RETURNTRANSFER, 1);
+		$locData = json_decode(curl_exec($goToSite), true);
+
+		$latitude = $locData["results"][0]["geometry"]["lat"];
+		$longitude = $locData["results"][0]["geometry"]["lng"];
+
+		echo "<p id='#hideLat'>" . $latitude . "</p>";
+		echo "<p id='#hideLon'>" . $longitude . "</p>";
+	?>
+
 		<h1>Results Page</h1>
 		<h2>You chose &ltInsert Country Here&gt</h2>
 
